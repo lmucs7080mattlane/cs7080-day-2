06d866f60844631c4ed5b6c476c26e3520dc7fc3
matt lane <m.lane@londonmet.ac.uk>
2017-02-19 15:22:35 +0000

    Broke server.py deliberately

--- server.py
+++ server.py
@@ -1,9 +1,25 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
+# Question 1:
+# The method below should be called by a web browser to get
+# the current web page.
+# Are you sure the following method should be a POST method?
+# The choices we have are GET, POST, PUT and DELETE
+@app.route('/', methods=['POST'])
 def hello():
-    return "Hello World!"
+    # Question 2:
+    # To print something out when you get a request for '/' route,
+    # uncomment the next line:
+
+    #print('Someone just called the "GET /" route')
+
+    # Question 3:
+    # To return a simple message, uncomment the next line.
+    # Perhaps change the message to something more interesting
+    
+    #return 'my simple message'
+    
 
 if __name__ == "__main__":
     app.run()
