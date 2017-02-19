8a2b2c7209d0bc2e5f574392270442eddcced08f
matt lane <m.lane@londonmet.ac.uk>
2017-02-19 15:35:53 +0000

    Fixed up server.py

--- server.py
+++ server.py
@@ -6,22 +6,22 @@ app = Flask(__name__)
 # the current web page.
 # Are you sure the following method should be a POST method?
 # The choices we have are GET, POST, PUT and DELETE
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def hello():
     # Question 2:
     # To print something out when you get a request for '/' route,
     # uncomment the next line:
 
-    #print('Someone just called the "GET /" route')
+    print('Someone just called the "GET /" route')
 
     # Question 3:
     # To return a simple message, uncomment the next line.
     # Perhaps change the message to something more interesting
     
-    #return 'my simple message'
+    return 'Hello mum, I\'m on TV'
     
     # ... and delete this line
-    pass
+
     
 
 if __name__ == "__main__":
