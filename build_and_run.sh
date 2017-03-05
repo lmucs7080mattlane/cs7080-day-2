523125fa22caf686c37ee1a254d26b93cb11eb42
matt lane <m.lane@londonmet.ac.uk>
2017-03-05 23:15:55 +0000

    Starting of IoT device server + database + web server example.

--- build_and_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker pull mongo:3.2
-docker rm -f mongo
-docker run --name mongo -v mongo:/data/db -d mongo
-
-docker build -t flask_hello_world .
-docker rm -f flask_hello_world
-docker run --name flask_hello_world -t -p 127.0.0.1:5000:5000 --link mongo -it flask_hello_world
