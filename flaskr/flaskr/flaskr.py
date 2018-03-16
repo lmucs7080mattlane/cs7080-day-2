136d7fa676526cf7bf69ef7377a2256c7c3481e2
IoT <iot@iot-VirtualBox.virtual.local>
2018-03-16 18:24:12 +0000

    this is my first commit. there are many commits like this but this one is mine. i changed some messages that appear whern you post a message or logout

--- flaskr/flaskr/flaskr.py
+++ flaskr/flaskr/flaskr.py
@@ -84,7 +84,7 @@ def add_entry():
     db.execute('insert into entries (title, text) values (?, ?)',
                [request.form['title'], request.form['text']])
     db.commit()
-    flash('New entry was successfully posted')
+    flash('thanks for the new post')
     return redirect(url_for('show_entries'))
 
 
@@ -106,5 +106,5 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
-    flash('You were logged out')
+    flash('Bye then')
     return redirect(url_for('show_entries'))
